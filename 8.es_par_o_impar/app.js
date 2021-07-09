bc5fac2c34ad1bab5a23bc4bc8ab3cbce101362b
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-09 14:40:03 -0300

    agregue ejercicio 08

---
+++ 8.es_par_o_impar/app.js
@@ -0,0 +1,14 @@
+const esParOImpar = (numero) => {
+    if( numero % 2 === 0) {
+        return "par"
+    }
+    else {
+        return "impar"
+    }
+}
+
+esParOImpar(10);
+esParOImpar(3);
+
+console.log(esParOImpar(10));
+console.log(esParOImpar(3));
